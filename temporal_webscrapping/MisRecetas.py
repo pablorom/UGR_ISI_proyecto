3b015443adc40bbccbe9f0c24ac13b3dcbf10e6d
CasillasAlba <albacaro@corre.ugr.es>
2021-04-26 18:19:57 +0200

    Aniade webscrapping (version inicial)

---
+++ temporal_webscrapping/MisRecetas.py
@@ -0,0 +1,33 @@
+# -*- coding: utf-8 -*-
+"""
+@author: Alba Casillas Rodríguez
+@author: Pablo Robles Molina
+@author: Tomás Ruíz Fernández
+"""
+
+"""
+    En esta clase se define los parámetros comunes de 
+    la información obtenida al usar WebScrapping y la API
+"""
+
+class MisRecetas:
+    
+    # Constructor de la clase Receta
+    def __init__(self,tit,img):
+        self.titulo = tit
+        self.urlImagen = img
+        #yo aniadiria un campo que indique el nombre de la pagina original
+        
+    # Definimos los get y set de las variables
+    def get_nombre(self):
+        return self.titulo
+    
+    def set_nombre(self, tit):
+        self.titulo = tit
+        
+    def get_urlImagen(self):
+        return self.urlImagen
+    
+    def set_urlImagen(self, imagen):
+        self.urlImagen = imagen
+        
---
+++ temporal_webscrapping/WebScrapping.py
@@ -0,0 +1,83 @@
+# -*- coding: utf-8 -*-
+"""
+@author: Alba Casillas Rodríguez
+@author: Pablo Robles Molina
+@author: Tomás Ruíz Fernández
+"""
+
+from bs4 import BeautifulSoup
+import requests
+import MisRecetas
+
+class WebScrapping:
+    
+    
+    # Constructor de la clase Receta
+    def __init__(self, ingred):
+        self.ingrediente = ingred
+        self.listaReceta = []
+        # self.ingrediente = ingrediente.replace("algo","+")
+        
+        
+    # Metodo que realice el WebScrapping de "Recetas Gratis"    
+    def buscar_recetasGratis(self):
+        url = 'https://www.recetasgratis.net/busqueda?q='
+        url_completa = url + self.ingrediente
+        html_text = requests.get(url_completa).text
+        soup = BeautifulSoup(html_text, 'lxml')
+        elementos = soup.find_all('div', class_ = 'resultado link')
+        
+        for elem in elementos:
+            #Obtenemos los parámetros deseados
+            titulo = elem.find('a', class_='titulo').text
+            imagen = elem.find('img', class_='imagen').get('data-imagen')
+            
+            # Aniadimos los parámetros obtenidos del webScrapping en una lista de
+            # elementos de tipo MisRecetas.
+            # Es necesario escribir "MisRecetas.MisRecetas" para llamar a la clase
+            # MisRecetas que está dentro del módulo con el mismo nombre
+            receta = MisRecetas.MisRecetas(titulo, imagen)
+            self.listaReceta.append(receta)
+            
+    
+    # Metodo que realice el WebScrapping de "No Solo Dulces"
+    def buscar_nosolodulces(self):
+         url = 'https://nosolodulces.es/?s='
+         url_completa = url + self.ingrediente
+         html_text = requests.get(url_completa).text
+         soup = BeautifulSoup(html_text, 'lxml')
+         elementos = soup.find_all('article', class_ = 'col-md-4 receta-categoria')
+         
+         for elem in elementos:
+            # Obtenemos los parámetros deseados
+            # Como el tag <a href...> donde se aloja el titulo no tiene ningún atributo 'class'
+            # no se puede hacer la búsqueda por la clase, de forma que buscamos todos los <a href>
+            # dentro del artículo y solo guardamos el atributo 'title'
+            titulo = elem.find('a', href=True).get('title')
+            imagen = elem.find('img', class_='img-destacada').get('src')
+
+            # Aniadimos los parámetros obtenidos del webScrapping en una lista de
+            # elementos de tipo MisRecetas.
+            # Es necesario escribir "MisRecetas.MisRecetas" para llamar a la clase
+            # MisRecetas que está dentro del módulo con el mismo nombre
+            receta = MisRecetas.MisRecetas(titulo, imagen)
+            self.listaReceta.append(receta)
+         
+         
+        
+        
+    # Método que muestra los resultados (para comprobar que funciona bien)
+    def mostrar_receta(self):
+        for lr in self.listaReceta:
+            print(lr.get_nombre())
+            print(lr.get_urlImagen())
+            print("")
+            
+    
+            
+            
+    
+ws = WebScrapping("pollo")
+ws.buscar_recetasGratis()
+ws.buscar_nosolodulces()
+ws.mostrar_receta()
