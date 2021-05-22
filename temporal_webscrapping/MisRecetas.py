197563ee8c2b256e4ce2d7838914919df9483d14
CasillasAlba <albacaro@corre.ugr.es>
2021-05-22 14:46:27 +0200

    Avanzada interfaz (categorias incorrectas)

--- temporal_webscrapping/MisRecetas.py
+++ temporal_webscrapping/MisRecetas.py
@@ -10,16 +10,21 @@
     la información obtenida al usar WebScrapping y la API
 """
 
-class MisRecetas:
+class MisRecetas(object):
     
     # Constructor de la clase Receta
-    def __init__(self,tit,img, orig):
+    def __init__(self,tit,img,orig,url):
         self.titulo = tit
         self.urlImagen = img
         self.paginaOriginal = orig
-        #yo aniadiria un campo que indique el nombre de la pagina original
+        self.urlReceta = url
+        self.tiempo = "No disponible"
+        self.comensales = "No disponible"
+        self.categoria = "No disponible"
+        self.ingredientes = []
         
-    # Definimos los get y set de las variables
+        
+    # Definimos los get y set de las variables   
     def get_nombre(self):
         return self.titulo
     
@@ -37,4 +42,38 @@ class MisRecetas:
     
     def set_paginaOriginal(self, orig):
         self.paginaOriginal = orig
+
+    def get_urlReceta(self):
+        return self.urlReceta
+    
+    def set_urlReceta(self, url):
+        self.urlReceta = url    
+
+    def get_tiempo(self):
+        return self.tiempo
+    
+    def set_tiempo(self, tmp):
+        self.tiempo = tmp
+
+    def get_comensales(self):
+        return self.comensales
+    
+    def set_comensales(self, px):
+        self.comensales = px
+
+    def get_categoria(self):
+        return self.categoria
+    
+    def set_categoria(self, cat):
+        self.categoria = cat
         
+    def get_ingredientes(self):
+        return self.ingredientes
+    
+    def add_ingrediente(self, ingr):
+        self.ingredientes.append(ingr)
+
+    def remove_ingredientes(self):
+        self.ingredientes.clear()
+
+    
